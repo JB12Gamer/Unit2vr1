af6850cf77deb0318affedb73607d73c28bf8903
JB12Gamer <111586949+JB12Gamer@users.noreply.github.com>
2022-11-10 11:30:50 -0700

    2.8

---
+++ 2.8/Assets/scripts/movement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public Animator animator;
+    float HorizontalSpeed = 0.01f;
+    public float m_Thrust = 400.0f;
+    Rigidbody2D m_Rigidbody;
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        float h = HorizontalSpeed * Input.GetAxis("Horizontal") * 3.5f;
+        animator.SetFloat("Speed", Mathf.Abs(h));
+        transform.Translate(h, 0, 0);
+        if (Input.GetKey("left"))
+        {
+
+            Vector2 scaleChange = new Vector2(-4.3f, 4.3f);
+            transform.localScale = scaleChange;
+        }
+        else if (Input.GetKey("right"))
+        {
+            Vector2 scaleChange = new Vector2(4.3f, 4.3f);
+            transform.localScale = scaleChange;
+        }
+        if (m_Rigidbody.velocity.y > 0.001 || m_Rigidbody.velocity.y < -0.001)
+        {
+            animator.SetBool("InAir", true);
+        }
+        else
+        {
+            animator.SetBool("InAir", false);
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                m_Rigidbody.AddForce(transform.up * m_Thrust);
+            }
+        }
+
+    }
+
+ 
+   
+}
